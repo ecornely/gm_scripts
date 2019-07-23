3277807765c3c7bc6b94b6ab4c2c2cc7ba922ee4
Eric Cornely <ericcornely@gmail.com>
2019-07-23 10:27:34 +0200

    Update HPSMSlash.js

--- HPSMSlash.js
+++ HPSMSlash.js
@@ -10,6 +10,7 @@
 // @include         http://hpsm.nrb.be
 // @include         https://hpsm.nrb.be/
 // @include         https://hpsm.nrb.be
+// @include         http://nrbnrw0237.nrb.be:8080/
 // @run-at          document-start
 // @version         0.0.1
 // ==/UserScript==
@@ -17,4 +18,4 @@
 if (window.top !== window.self) // NOTE: Do not run on iframes
     return;
 
-window.location='http://hpsm.nrb.be/webtier-9.35/index.do'
+window.location='http://hpsm.nrb.be/webtier-9.61/index.do'
