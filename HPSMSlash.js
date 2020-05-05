6e20e8452c7f5a712ed78ade2c3a4da337af6618
Eric Cornely <ericcornely@gmail.com>
2020-05-05 10:09:24 +0200

    Update HPSMSlash.js

--- HPSMSlash.js
+++ HPSMSlash.js
@@ -6,16 +6,23 @@
 // @namespace       contact@ecornely.be
 // @homepageURL     https://blog.ecornely.be
 // @supportURL      https://github.com/ecornely/gmscripts
-// @include         http://hpsm.nrb.be/
-// @include         http://hpsm.nrb.be
-// @include         https://hpsm.nrb.be/
-// @include         https://hpsm.nrb.be
-// @include         http://nrbnrw0237.nrb.be:8080/
+// @match         http://hpsm.nrb.be/
+// @match         http://hpsm.nrb.be
+// @match         https://hpsm.nrb.be/
+// @match         https://hpsm.nrb.be
+// @match         http://itsm.nrb.be/
+// @match         http://itsm.nrb.be
+// @match         https://itsm.nrb.be/
+// @match         https://itsm.nrb.be
+// @match         http://nrbnrw0237.nrb.be:8080/
 // @run-at          document-start
 // @version         0.0.1
 // ==/UserScript==
+(function() {
+  'use strict';
 
-if (window.top !== window.self) // NOTE: Do not run on iframes
+  if (window.top !== window.self){ // NOTE: Do not run on iframes
     return;
-
-window.location='http://hpsm.nrb.be/webtier-9.61/index.do'
+  }
+  window.location='http://hpsm.nrb.be/webtier-9.62/index.do'
+})();
